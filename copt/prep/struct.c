3c2553b3c453144d7140175945c827a0f512c0ba
Filipe Vasconcelos <filipe.manuel.vasconcelos@gmail.com>
2023-10-24 13:52:10 +0200

    new struct

--- copt/prep/struct.c
+++ copt/prep/struct.c
@@ -20,24 +20,14 @@ struct rect{
 };
 
 int main() {
-
     struct point p1={3,2};
     struct point p2={.x=4,.y=6};
-
     printf("%d %d\n",p1.x,p1.y);
-
     struct rect ecran;
-
     Point P1={.x=6,.y=2};
     Point* P2;
-    
     P2=&P1;
     P1.x=0;
     printf("%d %d\n",P2->x,P2->y); // (*P2).x <=> P2->x
-    
-
-
-
     //ecran.p1.x=
-
 }
