637fa0f8225f377fe1d9eee07a3e302e178bc514
Filipe Vasconcelos <filipe.manuel.vasconcelos@gmail.com>
2023-10-24 13:51:56 +0200

    struct enum

---
+++ copt/prep/struct.c
@@ -0,0 +1,43 @@
+// structure // typedef
+// pointeur de structure 
+// tableaux de structures
+
+#include<stdio.h>
+
+struct point{
+    int x;
+    int y;
+};
+
+typedef struct Point{
+    int x;
+    int y;
+} Point;
+
+struct rect{
+    struct point p1;
+    struct point p2;
+};
+
+int main() {
+
+    struct point p1={3,2};
+    struct point p2={.x=4,.y=6};
+
+    printf("%d %d\n",p1.x,p1.y);
+
+    struct rect ecran;
+
+    Point P1={.x=6,.y=2};
+    Point* P2;
+    
+    P2=&P1;
+    P1.x=0;
+    printf("%d %d\n",P2->x,P2->y); // (*P2).x <=> P2->x
+    
+
+
+
+    //ecran.p1.x=
+
+}
